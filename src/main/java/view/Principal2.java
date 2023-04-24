8f110987abc8a2dd78ba78642b51d9ab8590f870
JuanDavidFern <107356975+JuanDavidFern@users.noreply.github.com>
2023-04-24 18:32:55 +0200

    x

---
+++ src/main/java/view/Principal2.java
@@ -0,0 +1,99 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import model.Materia;
+
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import javax.swing.JComboBox;
+import java.awt.Insets;
+
+public class Principal2 extends JFrame {
+
+	private JPanel contentPane;
+	private static JComboBox<Materia> jcbMateria;
+	private JLabel lblNewLabel_1;
+	private  JComboBox jcbProfesor;
+	private JLabel lblNewLabel_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Principal2 frame = new Principal2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Principal2() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0};
+//		gbl_contentPane.columnWidths = new int[]{0};
+//		gbl_contentPane.rowHeights = new int[]{0};
+//		gbl_contentPane.columnWeights = new double[]{Double.MIN_VALUE};
+//		gbl_contentPane.rowWeights = new double[]{Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JLabel lblNewLabel = new JLabel("Materia");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+		
+		jcbMateria = new JComboBox<Materia>();
+		GridBagConstraints gbc_jcbMateria = new GridBagConstraints();
+		gbc_jcbMateria.insets = new Insets(0, 0, 5, 0);
+		gbc_jcbMateria.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jcbMateria.gridx = 1;
+		gbc_jcbMateria.gridy = 0;
+		contentPane.add(jcbMateria, gbc_jcbMateria);
+		
+		lblNewLabel_1 = new JLabel("Profesor");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 1;
+		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		jcbProfesor = new JComboBox();
+		GridBagConstraints gbc_jcbProfesor = new GridBagConstraints();
+		gbc_jcbProfesor.insets = new Insets(0, 0, 5, 0);
+		gbc_jcbProfesor.fill = GridBagConstraints.HORIZONTAL;
+		gbc_jcbProfesor.gridx = 1;
+		gbc_jcbProfesor.gridy = 1;
+		contentPane.add(jcbProfesor, gbc_jcbProfesor);
+		
+		lblNewLabel_2 = new JLabel("Nota");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 2;
+		contentPane.add(lblNewLabel_2, gbc_lblNewLabel_2);
+	}
+
+}
